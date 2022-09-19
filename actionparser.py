6ec18ba60c06c51b05d49ddf1918b2c880b3f726
Wonk <106038003+Wonkers0@users.noreply.github.com>
2022-09-19 01:11:29 -0500

    Grabbing action data from json

--- actionparser.py
+++ actionparser.py
@@ -12,58 +12,30 @@ from re import findall
 
 TOKEN = os.getenv("GH_TOKEN")
 g = Github(TOKEN)
-actionInfo = {
-  "actions": {},
-  "allActions": [],
-  "events": []
-}
+actionInfo = []
 
 print("Parsing Actions")
 # Get everything from github
 
 repo = g.get_repo('Wonkers0/DFSpigot')
 
-for file in repo.get_contents('utilities/actions'):
-  name = file.name.replace('.java', '')
-  actionInfo['actions'][name] = []
-  content = b64decode(file.content).decode('utf-8')
-  query = r'(?<=\n\t{4}case ").+(?=": {)'
-  if(name.startswith('If')): query = r'(?<=\n\t{3}case ").+(?=": {)'
-  for action in findall(query, content):
-    actionInfo['actions'][name].append(action)
-    actionInfo['allActions'].append(action)
-
-for event in findall(r'(?<=\s\s)[A-Z]\w+(?=: \[.*\])', b64decode(repo.get_contents('website/script.js').content).decode('utf-8')):
-  actionInfo['events'].append(event)
-
-
-# Write to a markdown file
-
-output = '''# Actions Support Dump
-This lists all actions mentioned in DFSpigot source code.
-
-## Actions'''
-
-for actionCategory in actionInfo['actions']:
-  output += '\n### ' + actionCategory + '\n'
-  for action in actionInfo['actions'][actionCategory]:
-    output += '* ' + action + '\n'
-
-output += '\n## Events\n'
-
-for event in actionInfo['events']:
-  output += '* ' + event + '\n'
+actionparams = repo.get_contents("DONT IMPORT/actionparams.json").decoded_content.decode('utf-8')
+matches = findall('"(.*?)": \[', actionparams)
+for match in matches:
+  if(len(match.split(":")) == 1):
+    continue
+  actionInfo.append(match.split(":")[1])
 
 # Export everything to files
 ## Create the action directory
 makedirs('action', exist_ok=True)
 
 md = open('./action/supportdump.md', 'w+', encoding='utf-8')
-md.write(output)
+md.write("\n".join(actionInfo))
 md.close()
 
 json = open('./action/supportdump.json', 'w+', encoding='utf-8')
-json.write(dumps(actionInfo))
+json.write(actionparams)
 json.close()
 
 print("Parsing Actions Complete")
